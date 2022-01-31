8f5aadc2f2ce6075e1ccc0e6c956d5e579d05207
yujinHan97 <dbwlslek97@gmail.com>
2022-01-31 16:03:57 +0900

    최종 완성 코드

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from "./About.module.css";
 
-function About() {
+function About(props) {
+    console.log(props);
     return (
         <div className={styles.about__container}>
             <span>
                 About this page: I built it because I love movies.
             </span>
             <span>
-                yujin
+                Made by Han Yujin
             </span>
         </div>
     );
